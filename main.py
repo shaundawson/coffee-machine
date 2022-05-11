cc61125f3e94047609c91550584422d60fdc5227
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-10 21:18:09 -0400

    make_coffee()

--- main.py
+++ main.py
@@ -95,4 +95,5 @@ while is_on:
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
             is_transaction_successful(payment,drink["cost"])
+            make_coffee(choice, drink["ingredients"])
 
